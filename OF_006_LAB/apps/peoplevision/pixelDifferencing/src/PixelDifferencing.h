c5505e7e010fe6370664166a217a4542c84d9217
adenine <adenine@gmail.com>
2009-10-08 13:47:46 +0000

    Updated differencing

---
+++ OF_006_LAB/apps/peoplevision/pixelDifferencing/src/PixelDifferencing.h
@@ -0,0 +1,20 @@
+/*
+ *  PixelDifferencing.h
+ *  openFrameworks
+ *
+ *  Created by Joshua Walton on 10/7/09.
+ *  Copyright 2009 Lab at Rockwell Group. All rights reserved.
+ *
+ */
+
+#ifndef PIXEL_DIFFERENCING
+#define PIXEL_DIFFERENCING
+
+class PixelDifferencing {
+	
+public:
+	PixelDifferencing();
+
+};
+
+#endif
