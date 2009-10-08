fc3b4026e0644c4a6daaf734732772f2a24be0c1
adenine <adenine@gmail.com>
2009-10-08 21:34:00 +0000

    Pixel Differencing changes

--- OF_006_LAB/apps/peoplevision/pixelDifferencing/src/testApp.h
+++ OF_006_LAB/apps/peoplevision/pixelDifferencing/src/testApp.h
@@ -1,9 +1,16 @@
 #ifndef _TEST_APP
 #define _TEST_APP
 
+#define SAMPLE_WIDTH		640
+#define SAMPLE_HEIGHT		480
 
 #include "ofMain.h"
 #include "PixelDifferencing.h"
+//#include "ofxVideoGrabber.h"
+#include "ofxOpenCv.h"
+
+//comment this out to use a normal USB cam
+//#define USING_OFX_VIDEOGRABBER
 
 class testApp : public ofBaseApp{
 
@@ -21,6 +28,32 @@ class testApp : public ofBaseApp{
 		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
 		void resized(int w, int h);
+	
+		#ifdef USING_OFX_VIDEOGRABBER
+			ofxVideoGrabber 		vidGrabber;
+		#else
+			ofVideoGrabber			vidGrabber;
+		#endif
+	
+		int 				camWidth;
+		int 				camHeight;
+		int                 appWidth;
+		int                 appHeight;
+	
+	ofxCvColorImage			colorLargeImage;
+	ofxCvColorImage			colorSmallImage;
+	ofxCvGrayscaleImage		grayLargeImage;
+	ofxCvGrayscaleImage 	graySmallImage;
+	ofxCvGrayscaleImage  bgImg;
+	
+	// images for frame differencing timer
+	ofxCvGrayscaleImage grayPrev;
+	ofxCvGrayscaleImage grayCurDiff;
+	int diffThreshold;
+		void checkDifferencing();
+	float motionThreshold;
+	bool learnedBackground; // whether it has grabbed a BG yet
+	
 
 };
 
