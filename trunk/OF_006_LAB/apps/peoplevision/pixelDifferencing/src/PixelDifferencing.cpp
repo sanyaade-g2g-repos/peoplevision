fa0625d773d9c0b7895d55e439cb239150e31a27
adenine@gmail.com <adenine@gmail.com@fbe84afc-b36d-11de-8e13-c326f17d7b4c>
2009-10-08 21:34:00 +0000

    Pixel Differencing changes

--- trunk/OF_006_LAB/apps/peoplevision/pixelDifferencing/src/PixelDifferencing.cpp
+++ trunk/OF_006_LAB/apps/peoplevision/pixelDifferencing/src/PixelDifferencing.cpp
@@ -8,7 +8,28 @@
  */
 
 #include "PixelDifferencing.h"
+#include "ofMain.h"
 
 PixelDifferencing::PixelDifferencing() {
 
 }
+
+void PixelDifferencing::setVideo(ofVideoGrabber tGrabber) {
+		//vGrabbber = tGrabber;
+}
+
+/*
+void PixelDifferencing::checkDifferencing() {
+		grayCurDiff.absDiff(grayPrev, grayLargeImage);   // curDiff is the difference between current and previous frame
+		cvThreshold(grayCurDiff.getCvImage(), grayCurDiff.getCvImage(), diffThreshold, 255, CV_THRESH_TOZERO); // anything below diffThreshold, drop to zero (compensate for noise)
+		int numPixelsChanged = grayCurDiff.countNonZeroInRegion(0, 0, WIDTH, HEIGHT); // this is how many pixels have changed that are above diffThreshold
+			
+		if (numPixelsChanged > motionThreshold && numPixelsChanged < motionThreshold + 10000) {
+			overLayer->loadSet();
+			currentState = 1;
+			cout<<"CHANGED BECAUSE: "<<numPixelsChanged<<endl;
+		}
+		
+		grayPrev = grayLargeImage; // save current frame for next loop
+}
+*/
--- trunk/OF_006_LAB/apps/peoplevision/pixelDifferencing/src/PixelDifferencing.h
+++ trunk/OF_006_LAB/apps/peoplevision/pixelDifferencing/src/PixelDifferencing.h
@@ -10,11 +10,30 @@
 #ifndef PIXEL_DIFFERENCING
 #define PIXEL_DIFFERENCING
 
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxVideoGrabber.h"
+
+
 class PixelDifferencing {
 	
 public:
 	PixelDifferencing();
-
+	void setVideo(ofVideoGrabber tGrabber);
+	void checkDifferencing();
+	
+	ofxCvColorImage			colorLargeImage;
+	ofxCvColorImage			colorSmallImage;
+	ofxCvGrayscaleImage		grayLargeImage;
+	ofxCvGrayscaleImage 	graySmallImage;
+	ofxCvGrayscaleImage		bgImg;
+	
+	// images for frame differencing timer
+	ofxCvGrayscaleImage grayPrev;
+	ofxCvGrayscaleImage grayCurDiff;
+	
+	ofVideoGrabber vGrabbber;
+	
 };
 
 #endif
