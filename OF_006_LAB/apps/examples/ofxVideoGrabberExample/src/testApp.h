788084d0cdc7139ee1bda025b4778d5a1b9835eb
robotconscience <robotconscience@gmail.com>
2009-10-08 21:30:25 +0000

    ofxtouch_simpleblobtracker + ofxvideograbber

--- OF_006_LAB/apps/examples/ofxVideoGrabberExample/src/testApp.h
+++ OF_006_LAB/apps/examples/ofxVideoGrabberExample/src/testApp.h
@@ -1,48 +1,33 @@
-#ifndef _TEST_APP
-#define _TEST_APP
-
-
-#include "ofMain.h"
-#include "ofxVideoGrabber.h"
-
-//comment this out to use a normal USB cam
 #define USING_OFX_VIDEOGRABBER
+#define BLOB_TRACKER_VIDEO_FORMAT VID_FORMAT_GREYSCALE
+#define BLOB_TRACKER_COLOR_FORMAT VID_FORMAT_GREYSCALE
 
-class testApp : public ofBaseApp{
+#include "ofxTouchApp.h"
+#include "ofMain.h"
 
-	public:
+class testApp : public ofBaseApp {
 
-		void setup();
-		void update();
-		void draw();
-
-		void calculateCaptureFramerate();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-
-	#ifdef USING_OFX_VIDEOGRABBER
-		ofxVideoGrabber 		vidGrabber;
-	#else
-		ofVideoGrabber			vidGrabber;
-	#endif
+  public:
+  
+	ofxTouchApp touchHandler;
 	
-		int 				camWidth;
-		int 				camHeight;
-		int                 appWidth;
-		int                 appHeight;
+    bool bDragging;
 
-        /*framerate display*/
-		char                buf[255];
-		char                buf2[255];
-		float 	            mytimeNow, mytimeThen, myframeRate;
-		float 	            myfps;
-		float                 myframes;
+    void setup();
+    void update();
+    void draw();
+
+    void keyPressed  (int key);
+    void keyReleased  (int key);
+
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased();
+    
+    void fingerDragged( ofxTouchFinger & finger );
+    void fingerPressed( ofxTouchFinger & finger );    
+    void fingerReleased( ofxTouchFinger & finger );
+    
 };
 
-#endif
