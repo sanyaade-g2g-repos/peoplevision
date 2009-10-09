4981be9bf9a16cce356b1123be131b0f8e2c4f45
robotconscience@gmail.com <robotconscience@gmail.com@fbe84afc-b36d-11de-8e13-c326f17d7b4c>
2009-10-09 18:09:23 +0000

    progressive background capture app finished

--- trunk/OF_006_LAB/apps/peoplevision/progressiveBackground/src/testApp.h
+++ trunk/OF_006_LAB/apps/peoplevision/progressiveBackground/src/testApp.h
@@ -1,24 +1,52 @@
 #ifndef _TEST_APP
 #define _TEST_APP
 
+#define _USE_LIVE_VIDEO
+
+/******************************************************************
+	TRACKING + FILTER SCRIPT FROM NUI CCV
+******************************************************************/
 
 #include "ofMain.h"
+#include "ofxOpenCv.h"
 
-
-class testApp : public ofSimpleApp{
+class testApp : public ofBaseApp{
 	public:
+
 		void setup();
 		void update();
 		void draw();
 		
 		void keyPressed  (int key);
-		void keyReleased (int key);
 		
 		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
-		void mouseReleased();
+		void mouseReleased(int x, int y, int button);
+			
+		#ifdef _USE_LIVE_VIDEO
+			ofVideoGrabber 		vidGrabber;
+		#else
+			ofVideoPlayer 		vidPlayer;
+		#endif
 	
+		ofxCvColorImage		colorImg;
+	
+		ofxCvGrayscaleImage 	grayImage;
+	
+		ofxCvGrayscaleImage grayBg;
+		ofxCvGrayscaleImage subtractBg;
+		ofxCvGrayscaleImage grayDiff;
+		ofxCvShortImage		floatBgImg;
+	
+		ofxCvContourFinder 	contourFinder;
+	
+		int 				threshold;
+		bool				bLearnBakground;
+		bool				bLearnBakgroundProgressive;
+		bool				bTrackDark;
+		float				fLearnRate;
+		float camWidth, camHeight;
 };
 
 #endif
