bfd92f121f76bac8f84c8598c59dfccf3ef3f780
robotconscience@gmail.com <robotconscience@gmail.com@fbe84afc-b36d-11de-8e13-c326f17d7b4c>
2009-10-13 16:11:20 +0000

    peopleTracker version 000 inital add

--- trunk/OF_006_LAB/apps/peoplevision/progressiveBackground/src/testApp.h
+++ trunk/OF_006_LAB/apps/peoplevision/progressiveBackground/src/testApp.h
@@ -3,17 +3,22 @@
 
 #define _USE_LIVE_VIDEO
 
-/******************************************************************
-	TRACKING + FILTER SCRIPT FROM NUI CCV
-******************************************************************/
-
 #include "ofMain.h"
 #include "ofxOpenCv.h"
 #include "ofxCvHaarTracker.h"
 #include "ofxSimpleGuiToo.h"
 #include "CPUImageFilter.h"
+#include "ofxPerson.h"
 
 #ifdef _USE_LIVE_VIDEO
+
+//	look in ofxVideoGrabber to find out about more formats
+//	comment this out if you just want to use the normal quicktime stuff
+
+//	NOTE: WE MAY NEED MORE STUFF TO MAKE THIS WIN COMPATIBLE
+//	ofxVideoGrabber has some parts that will work, but if you want to
+//	use the normal ofVideoGrabber with Win, we'll need to incorporate
+//	utils from CCV for PS3 eye, etc
 
 #define USING_OFX_VIDEOGRABBER
 #define BLOB_TRACKER_VIDEO_FORMAT VID_FORMAT_GREYSCALE
@@ -32,6 +37,8 @@
 #endif
 
 #endif
+
+// display + calibration modes : press 'm' to cycle between them
 
 #define MODE_NORMAL				0
 #define MODE_GUI				1
@@ -61,14 +68,22 @@ class testApp : public ofBaseApp{
 			ofVideoPlayer 		vidPlayer;
 		#endif
 	
+	//CV images for detection + processing
 	
-#ifdef USING_OFX_VIDEOGRABBER
-	ofxCvGrayscaleImage		colorImg;
-	ofxCvGrayscaleImage		colorSmallImage;
-#else
-	ofxCvColorImage		colorImg;
-	ofxCvColorImage		colorSmallImage;
-#endif
+	#if BLOB_TRACKER_COLOR_FORMAT == VID_FORMAT_GREYSCALE
+		
+	// if you're capturing greyscale images from ofxVideoGrabber
+	// you need to only look at greyscale images
+	
+		ofxCvGrayscaleImage		colorImg;
+		ofxCvGrayscaleImage		colorSmallImage;
+		ofxCvGrayscaleImage		colorBabyImage;
+	
+	#else
+		ofxCvColorImage		colorImg;
+		ofxCvColorImage		colorSmallImage;
+		ofxCvColorImage		colorBabyImage;
+	#endif
 	
 		ofxCvGrayscaleImage	grayImage;
 		ofxCvGrayscaleImage	grayLastImage;
@@ -76,12 +91,22 @@ class testApp : public ofBaseApp{
 		ofxCvGrayscaleImage grayBg;
 		ofxCvGrayscaleImage subtractBg;
 		ofxCvGrayscaleImage graySmallImage;
+		ofxCvGrayscaleImage grayBabyImage;
+	
+	//more specific CV images for processing
+	
 		CPUImageFilter		grayDiff;
 		ofxCvShortImage		floatBgImg;
 	
-		ofxCvContourFinder 	contourFinder;
+	// blob tracking things
 	
+		ofxCvContourFinder 	contourFinder;
 		int mode;
+	
+	//filter variables
+	
+		float fLearnRate;
+		float camWidth, camHeight;
 	
 		int smooth;
 		int highpassBlur;
@@ -91,12 +116,19 @@ class testApp : public ofBaseApp{
 		int minBlob;
 		int maxBlob;
 	
+	// haar
+	
 		float haarArea;
 		float minHaarArea;
 		float maxHaarArea;
+		ofxCvHaarFinder			haarFinder;
+		ofxCvHaarTracker		haarTracker;
 	
-		float fLearnRate;
-		float camWidth, camHeight;
+	// optical flow
+	
+		ofxCvOpticalFlowLK	opticalFlow;
+	
+	// switches for filters
 		
 		bool bFindHoles;
 		bool bLearnBackground;
@@ -108,11 +140,6 @@ class testApp : public ofBaseApp{
 		bool bSmooth;
 		bool bDetectHaar;
 		bool bTrackOpticalFlow;
-	
-		ofxCvHaarFinder			haarFinder;
-		ofxCvHaarTracker		haarTracker;
-	
-		ofxCvOpticalFlowLK	opticalFlow;
 	
 		ofxSimpleGuiToo	gui;
 };
