04d6a7c8c079ddc65756f5d8260abf1aef88b72a
backsmith <backsmith@fbe84afc-b36d-11de-8e13-c326f17d7b4c>
2009-12-05 23:22:40 +0000

    Deleted extra files in examples folder

--- OF_006_LAB/apps/examples/gamuza002.0[beta]/src/ofxGamuza/src/imageFilters/ImageFilters.h
+++
@@ -1,8 +0,0 @@
-#ifndef _OF_IMAGE_FILTERS
-#define _OF_IMAGE_FILTERS
-
-#include "ofMain.h"
-
-void boxBlur(unsigned char *pix, int w, int h, int radius);
-
-#endif
--- OF_006_LAB/apps/examples/gamuza002.0[beta]/src/ofxPachube/src/ofxPachube.h
+++
@@ -1,7 +0,0 @@
-#ifndef OFX_PACHUBE_H
-#define OFX_PACHUBE_H
-
-#include "ofxPachubeInput.h"
-#include "ofxPachubeOutput.h"
-
-#endif
